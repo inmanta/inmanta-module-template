81fe5bfcb975a4c9b54726f42b7cb6f234cfddaf
Lejoly Florent <florent.lejoly@inmanta.com>
2022-01-06 17:13:11 +0100

    y

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -2,5 +2,5 @@
 {{ cookiecutter.update({"inmanta_module_name": (cookiecutter.module_name[0].lower()+cookiecutter.module_name[1:]).replace("-","_") }) }}
 
 print("===========================")
-print("module name: {{ cookiecutter.inmanta_module_name }}")
+print("Module name: {{ cookiecutter.inmanta_module_name }}")
 print("Python package name: {{ cookiecutter.package_name }}")
