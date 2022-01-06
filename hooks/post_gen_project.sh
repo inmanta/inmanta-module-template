d6326d0556d474463f21316cc0e4954e4db11188
Lejoly Florent <florent.lejoly@inmanta.com>
2022-01-06 17:06:40 +0100

    wip

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env sh
 
 # don't abort cookiecutter if git is not installed or not configured properly (i.e. no email address configured)
-git init || true
-git add . || true
-git commit -m "initial commit: cookiecutter template" || true
+git init > /dev/null || true
+git add . > /dev/null || true
+git commit -m "initial commit: cookiecutter template" > /dev/null && echo "Git initialized with initial commit: cookiecutter template" || true
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,2 +1,6 @@
 {{ cookiecutter.update({"package_name": "inmanta-module-"+cookiecutter.module_name.lower().replace("_","-") }) }}
 {{ cookiecutter.update({"inmanta_module_name": (cookiecutter.module_name[0].lower()+cookiecutter.module_name[1:]).replace("-","_") }) }}
+
+print("===========================")
+print("module name: {{ cookiecutter.inmanta_module_name }}")
+print("Python package name: {{ cookiecutter.package_name }}")
