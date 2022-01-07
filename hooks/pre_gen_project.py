defe92714677645fd96c0cfbf8eb880c1afc3cdf
Lejoly Florent <florent.lejoly@inmanta.com>
2022-01-07 15:41:47 +0100

    e

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,6 +1,12 @@
+"""
+    This script has as purpose to create the Python package name and the 
+    'Inmanta module name' using the module name inputed by the user (or the default module name)
+    The Python package name uses the same naming convention as standart Python packages: 
+    all lowercase and words separated by "_".
+    The 'Inmanta module name' (which is also the name of the Python module name) uses "-" to separate
+    words and can have uppercase letters except for the first letter which is always lowercase.
+
+"""
+
 {{ cookiecutter.update({"package_name": "inmanta-module-" + cookiecutter.module_name.lower().replace("_","-") }) }}
 {{ cookiecutter.update({"inmanta_module_name": (cookiecutter.module_name[0].lower() + cookiecutter.module_name[1:]).replace("-","_") }) }}
-
-print("===========================")
-print("Module name: {{ cookiecutter.inmanta_module_name }}")
-print("Python package name: {{ cookiecutter.package_name }}")
