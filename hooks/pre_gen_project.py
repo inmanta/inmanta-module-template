0d2b433c5b431b06541f0c0ff0652b450584384a
FloLey <95339363+FloLey@users.noreply.github.com>
2022-01-11 10:59:34 +0100

    Update package and module names for Modules V2 (#21)
    make sure - and _ are converted correctly in the module name: the Python package name should use - while all other names (even the Python module name and directory) should use _.
    
    make sure capital letters are dealt with:
    
    - a capital letter at the start of a module name should be converted to a lower case letter
    
    - capital letters in other positions of the module name should not be converted
    
    issue #14

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,12 @@
+"""
+    This script has as purpose to create the Python package name and the 
+    'Inmanta module name' using the module name input (or the default module name).
+    The Python package name uses the same naming convention as standard Python packages: 
+    all lowercase and words separated by "-".
+    The 'Inmanta module name' (which is also the name of the Python module name) uses "_" to separate
+    words and can have uppercase letters except for the first letter which is always lowercase.
+
+"""
+
+{{ cookiecutter.update({"package_name": "inmanta-module-" + cookiecutter.module_name.lower().replace("_","-") }) }}
+{{ cookiecutter.update({"inmanta_module_name": (cookiecutter.module_name[0].lower() + cookiecutter.module_name[1:]).replace("-","_") }) }}
